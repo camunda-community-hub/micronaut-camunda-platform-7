316ff537f18067f796ccc3678b17d5f6d09fee7f
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2020-03-27 21:13:54 +0100

    Enable duplicateFiltering to reduce number of deployments.

--- micronaut-camunda-bpm-example/src/main/java/info/novatec/micronaut/camunda/bpm/example/LoggerDelegate.java
+++ micronaut-camunda-bpm-example/src/main/java/info/novatec/micronaut/camunda/bpm/example/LoggerDelegate.java
@@ -2,14 +2,18 @@ package info.novatec.micronaut.camunda.bpm.example;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Singleton;
 
 @Singleton
 public class LoggerDelegate implements JavaDelegate {
 
+    private static final Logger log = LoggerFactory.getLogger(LoggerDelegate.class);
+
     @Override
     public void execute(DelegateExecution delegateExecution) {
-        System.out.println("Hello World: " + delegateExecution);
+        log.info("Hello World: {}", delegateExecution);
     }
 }
