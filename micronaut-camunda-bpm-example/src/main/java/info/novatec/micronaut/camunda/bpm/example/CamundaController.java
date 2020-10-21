f85dc794636013cee3cfe94d89cb28a618e9f2d9
Hayvon <aleksandrarshavskiy@gmail.com>
2020-10-21 13:07:45 +0200

    Added reproducible loadtest (Loadtest.jmx). The load test sends 100 simultaneous requests (10000 in total) to an endpoint (/camunda/start-hello-world-process).

--- micronaut-camunda-bpm-example/src/main/java/info/novatec/micronaut/camunda/bpm/example/CamundaController.java
+++ micronaut-camunda-bpm-example/src/main/java/info/novatec/micronaut/camunda/bpm/example/CamundaController.java
@@ -1,13 +1,12 @@
 package info.novatec.micronaut.camunda.bpm.example;
 
 import io.micronaut.http.MediaType;
-import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.annotation.*;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.repository.ResourceDefinition;
 
 import java.util.stream.Collectors;
@@ -19,9 +18,12 @@ public class CamundaController {
 
     private final RepositoryService repositoryService;
 
-    public CamundaController(ProcessEngine processEngine, RepositoryService repositoryService) {
+    private final RuntimeService runtimeService;
+
+    public CamundaController(ProcessEngine processEngine, RepositoryService repositoryService, RuntimeService runtimeService) {
         this.processEngine = processEngine;
         this.repositoryService = repositoryService;
+        this.runtimeService = runtimeService;
     }
 
     @Get("/name")
@@ -39,4 +41,11 @@ public class CamundaController {
                 .map(ResourceDefinition::getKey)
                 .collect(Collectors.joining());
     }
+
+    @Post("/hello-world-process")
+    @ExecuteOn(TaskExecutors.IO)
+    public String startHelloWorldProcess() {
+        return runtimeService.startProcessInstanceByKey("HelloWorld").getId();
+    }
+
 }
