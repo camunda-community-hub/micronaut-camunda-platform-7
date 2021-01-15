2069640dffcd5045ff7b92cbe4243c2c0baf5961
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2021-01-15 06:44:26 +0100

    Add test to check that the Camunda BPM version is logged on startup

---
+++ micronaut-camunda-bpm-feature/src/test/kotlin/info/novatec/micronaut/camunda/bpm/feature/test/ProcessEngineFactoryLoggerTest.kt
@@ -0,0 +1,34 @@
+package info.novatec.micronaut.camunda.bpm.feature.test
+
+import ch.qos.logback.classic.Logger
+import info.novatec.micronaut.camunda.bpm.feature.ProcessEngineFactory
+import io.micronaut.context.ApplicationContext
+
+import org.junit.jupiter.api.Test
+import ch.qos.logback.core.read.ListAppender
+import org.junit.jupiter.api.Assertions.assertTrue
+
+import ch.qos.logback.classic.spi.ILoggingEvent
+import org.slf4j.LoggerFactory
+
+/**
+ * @author Tobias Schäfer
+ */
+class ProcessEngineFactoryLoggerTest {
+
+    @Test
+    fun `Camunda BPM version is logged on application start`() {
+
+        val logger = LoggerFactory.getLogger(ProcessEngineFactory::class.java) as Logger
+
+        val listAppender = ListAppender<ILoggingEvent>()
+        listAppender.start()
+
+        logger.addAppender(listAppender)
+
+        ApplicationContext.run()
+
+        assertTrue(listAppender.list.stream()
+            .anyMatch { e -> e.formattedMessage.contains(Regex("""Camunda BPM version: \d+\.\d+\.\d+""")) })
+    }
+}
