1bec20bb8d151686207f0f3549d286bd759161c3
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2021-01-18 19:52:03 +0100

    Replace interface instead of default implementation for customizers.

--- micronaut-camunda-bpm-feature/src/main/java/info/novatec/micronaut/camunda/bpm/feature/JobExecutorCustomizer.java
+++ micronaut-camunda-bpm-feature/src/main/java/info/novatec/micronaut/camunda/bpm/feature/JobExecutorCustomizer.java
@@ -1,9 +1,12 @@
 package info.novatec.micronaut.camunda.bpm.feature;
 
+import io.micronaut.context.annotation.DefaultImplementation;
+
 /**
  * @author Aleksandr Arshavskiy
  */
 @FunctionalInterface
+@DefaultImplementation(DefaultJobExecutorCustomizer.class)
 public interface JobExecutorCustomizer {
 
     void customize(MnJobExecutor jobExecutor);
--- micronaut-camunda-bpm-feature/src/test/kotlin/info/novatec/micronaut/camunda/bpm/feature/test/MicronautProcessEngineConfigurationCustomizer.kt
+++ micronaut-camunda-bpm-feature/src/test/kotlin/info/novatec/micronaut/camunda/bpm/feature/test/MicronautProcessEngineConfigurationCustomizer.kt
@@ -1,15 +1,12 @@
 package info.novatec.micronaut.camunda.bpm.feature.test
 
-import info.novatec.micronaut.camunda.bpm.feature.DefaultProcessEngineConfigurationCustomizer
 import info.novatec.micronaut.camunda.bpm.feature.MnProcessEngineConfiguration
 import info.novatec.micronaut.camunda.bpm.feature.ProcessEngineConfigurationCustomizer
 import io.micronaut.context.annotation.Replaces
-import org.camunda.bpm.engine.ProcessEngineConfiguration
-import javax.annotation.Nonnull
 import javax.inject.Singleton
 
 @Singleton
-@Replaces(DefaultProcessEngineConfigurationCustomizer::class)
+@Replaces(ProcessEngineConfigurationCustomizer::class)
 class MicronautProcessEngineConfigurationCustomizer : ProcessEngineConfigurationCustomizer {
     override fun customize(processEngineConfiguration: MnProcessEngineConfiguration) {
         processEngineConfiguration.processEngineName = PROCESS_ENGINE_NAME
