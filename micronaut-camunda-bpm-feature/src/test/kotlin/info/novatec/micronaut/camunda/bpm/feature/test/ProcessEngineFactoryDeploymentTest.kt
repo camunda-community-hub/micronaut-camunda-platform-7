3ca66a328f6cb1621d88418e8e25f5c2764f83a5
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2022-02-23 21:59:11 +0100

    Verify that DMN gets deployed

--- micronaut-camunda-bpm-feature/src/test/kotlin/info/novatec/micronaut/camunda/bpm/feature/test/ProcessEngineFactoryDeploymentTest.kt
+++ micronaut-camunda-bpm-feature/src/test/kotlin/info/novatec/micronaut/camunda/bpm/feature/test/ProcessEngineFactoryDeploymentTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 original authors
+ * Copyright 2021-2022 original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,5 +50,11 @@ class ProcessEngineFactoryDeploymentTest {
     fun `camunda forms get deployed`() {
         assertThat(repositoryService.getDeploymentResourceNames("1"))
             .contains("sample.form")
+    }
+
+    @Test
+    fun `DMN tables get deployed`() {
+        assertThat(repositoryService.getDeploymentResourceNames("1"))
+            .contains("simple-table.dmn")
     }
 }
