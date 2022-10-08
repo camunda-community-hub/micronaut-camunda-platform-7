51a5d20ee2cfbe58ee5447594ab3f709d0487082
Tobias Schaefer <tobias.schaefer@envite.de>
2022-10-08 20:16:53 +0200

    Update to Camunda 7.18

--- micronaut-camunda-bpm-feature/src/main/java/info/novatec/micronaut/camunda/bpm/feature/eventing/PublishDelegateParseListener.java
+++ micronaut-camunda-bpm-feature/src/main/java/info/novatec/micronaut/camunda/bpm/feature/eventing/PublishDelegateParseListener.java
@@ -23,6 +23,7 @@ import org.camunda.bpm.engine.delegate.ExecutionListener;
 import org.camunda.bpm.engine.delegate.TaskListener;
 import org.camunda.bpm.engine.impl.bpmn.behavior.UserTaskActivityBehavior;
 import org.camunda.bpm.engine.impl.bpmn.parser.BpmnParseListener;
+import org.camunda.bpm.engine.impl.core.variable.mapping.IoMapping;
 import org.camunda.bpm.engine.impl.persistence.entity.ProcessDefinitionEntity;
 import org.camunda.bpm.engine.impl.pvm.process.ActivityImpl;
 import org.camunda.bpm.engine.impl.pvm.process.ScopeImpl;
@@ -254,6 +255,11 @@ public class PublishDelegateParseListener implements BpmnParseListener {
     @Override
     public void parseConditionalStartEventForEventSubprocess(Element element, ActivityImpl conditionalActivity, boolean interrupting) {
         // Do not implement. Start and end event listener are set by parseBoundaryEvent()
+    }
+
+    @Override
+    public void parseIoMapping(Element extensionElements, ActivityImpl activity, IoMapping inputOutput) {
+        // Do not implement
     }
 
     @Override
